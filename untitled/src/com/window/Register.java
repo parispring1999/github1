6b97096d84ef446dc425a4f89dd46dc6f84139f3
parispring1999 <3059655099@qq.com>
2022-02-17 04:25:17 +0800

    修改4

---
+++ untitled/src/com/window/Register.java
@@ -0,0 +1,44 @@
+package com.window;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.FlowLayout.CENTER;
+
+public class Register extends JFrame {
+    final int WIDTH=500;
+    final int HEIGHT=200;
+    FlowLayout flowlayout;
+    JPanel jpanel_1;
+    JLabel bgimg;
+
+    //构造器
+    public Register(){
+        init();
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        validate();
+    }
+    void init(){
+        //居中对齐
+        flowlayout = new FlowLayout(CENTER);
+        //窗口位置和大小
+        Toolkit kit=Toolkit.getDefaultToolkit();
+        Dimension screenSize=kit.getScreenSize();
+        int width=screenSize.width;
+        int height=screenSize.height;
+        int x=(width-WIDTH)/2;
+        int y=(height-HEIGHT)/2;
+        this.setBounds(x,y,WIDTH,HEIGHT);
+        this.setLayout(null);
+        //盘子大小
+        jpanel_1=new JPanel();
+        jpanel_1.setBounds(0,0,width,height);
+        jpanel_1.setLayout(flowlayout);
+        //背景
+        ImageIcon img=new ImageIcon("src/img/2.jpg");
+        bgimg=new JLabel(img);
+        bgimg.setBounds(0,0,img.getIconWidth(),img.getIconHeight());
+    }
+}
--- untitled/src/com/window/StudentSystem.java
+++ untitled/src/com/window/StudentSystem.java
@@ -3,5 +3,6 @@ package com.window;
 public class StudentSystem {
     public static void main(String[] args){
         LoginStart loginstart=new LoginStart();
+        Register register=new Register();
     }
 }
