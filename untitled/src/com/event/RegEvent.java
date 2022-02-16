d9288de411e857483258851f3984f4fca1c24a06
parispring1999 <3059655099@qq.com>
2022-02-17 02:57:33 +0800

    修改2

--- untitled/src/com/event/RegEvent.java
+++ untitled/src/com/event/RegEvent.java
@@ -1,4 +1,29 @@
 package com.event;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+public class RegEvent implements MouseListener{
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.print("点击");
+    }
 
-public class RegEvent {
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
 }
